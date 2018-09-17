5d87042b7757f654545997416b11b7aea1ed012f
RamposhPidrov <40067659+RamposhPidrov@users.noreply.github.com>
2018-09-17 09:57:12 +0500

    Update task1.sql

--- Lab5.1__/task1.sql
+++ Lab5.1__/task1.sql
@@ -1,1 +1,12 @@
 SELECT if(model regexp '^([A-z]*|[0-9]*)$', model, null) FROM product;
+
+
+SELECT 
+    ko.still
+FROM
+    (SELECT 
+        IF(model REGEXP '^([A-z]*|[0-9]*)$', model, NULL) AS still
+    FROM
+        product) AS ko
+WHERE
+    ko.still IS NOT NULL;
