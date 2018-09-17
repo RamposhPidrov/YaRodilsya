3ffd8d8658f6ef25fde909e3d26e19d4ed482aa5
RamposhPidrov <40067659+RamposhPidrov@users.noreply.github.com>
2018-09-17 10:33:07 +0500

    Update task2.sql

--- Lab5.1__/task2.sql
+++ Lab5.1__/task2.sql
@@ -13,3 +13,29 @@ FROM
     GROUP BY subP.maker
     ORDER BY count DESC , maker) AS subP, (SELECT @i:=0) AS t) AS subQ ON subQ.maker = P.maker
 
+SELECT 
+    C.num, P.maker, P.model
+FROM
+    product AS P
+        LEFT JOIN
+    (SELECT 
+        (SELECT 
+                    COUNT(*)
+                FROM
+                    (SELECT 
+                    COUNT(model) AS count, maker
+                FROM
+                    product
+                GROUP BY maker
+                ORDER BY count DESC , maker) AS B
+                WHERE
+                    (B.count >= A.count
+                        AND IF(B.count = A.count, B.maker <= A.maker, B.count))) AS num,
+            A.maker
+    FROM
+        (SELECT 
+        COUNT(model) AS count, maker
+    FROM
+        product
+    GROUP BY maker
+    ORDER BY count DESC , maker) AS A) AS C ON C.maker = P.maker
