80ddbc4911ba480028ee735c10b395fd386cd634
RamposhPidrov <40067659+RamposhPidrov@users.noreply.github.com>
2018-09-18 20:03:47 +0500

    Update task2.sql

--- Lab5.1__/task2.sql
+++ Lab5.1__/task2.sql
@@ -14,28 +14,36 @@ FROM
     ORDER BY count DESC , maker) AS subP, (SELECT @i:=0) AS t) AS subQ ON subQ.maker = P.maker
 
 SELECT 
-    C.num, P.maker, P.model
+    C.num, P.maker, P.model 
 FROM
     product AS P
-        LEFT JOIN
-    (SELECT 
+    LEFT JOIN
         (SELECT 
-                    COUNT(*)
-                FROM
-                    (SELECT 
+            (SELECT 
+                COUNT(*)
+             FROM
+                (SELECT 
                     COUNT(model) AS count, maker
-                FROM
+                 FROM
                     product
-                GROUP BY maker
-                ORDER BY count DESC , maker) AS B
-                WHERE
-                    (B.count >= A.count
-                        AND IF(B.count = A.count, B.maker <= A.maker, B.count))) AS num,
-            A.maker
-    FROM
-        (SELECT 
-        COUNT(model) AS count, maker
-    FROM
-        product
-    GROUP BY maker
-    ORDER BY count DESC , maker) AS A) AS C ON C.maker = P.maker
+                 GROUP BY maker
+                 ORDER BY count DESC , maker) AS B
+              WHERE
+                (B.count >= A.count
+                AND IF(B.count = A.count, B.maker <= A.maker, B.count))) AS num,
+              A.maker
+         FROM
+            (SELECT 
+                COUNT(model) AS count, maker  
+            FROM
+                product
+            GROUP BY maker
+            ORDER BY count DESC , maker) AS A)
+        AS C ON C.maker = P.maker
+
+#Выбирается номер из топа, имя, модель, которую производит производитель
+#Из выборки топа, который формируется по количеству произведенных моделей
+#Топ нумеруется подзапросом с условием в WHERE
+#Основное условие - по количеству моделей
+#Вторичное условие - если у производителей одинакове количество моделей, то нумеровка идет по имени, иначе так же по каунту
+#Я поплавлен
